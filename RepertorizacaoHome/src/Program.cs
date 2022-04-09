c691551205c4e4140cbf1123d677e8e56d65172a
Lucas Lauri <lucaslauri@hotmail.com>
2022-04-09 14:49:09 -0300

    .

--- RepertorizacaoHome/src/Program.cs
+++ RepertorizacaoHome/src/Program.cs
@@ -9,6 +9,7 @@ using System.Deployment.Application;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Security;
 using System.Security.Permissions;
@@ -23,6 +24,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace RepertorizacaoHome.src
@@ -103,7 +105,7 @@ namespace RepertorizacaoHome.src
         public Program()
         {
 
-            //System.Diagnostics.Debugger.Launch();
+            System.Diagnostics.Debugger.Launch();
 
             //Directory.CreateDirectory(MainPath);
 
@@ -510,6 +512,47 @@ namespace RepertorizacaoHome.src
 
             UpdatesMedicinesView();
         }
+        private bool CheckForUpdateAvailable()
+        {
+            Uri updateLocation = ApplicationDeployment.CurrentDeployment.UpdateLocation;
+
+            //Used to use the Clickonce API but we've uncovered a pretty serious bug which results in a COMException and the loss of ability
+            //to check for updates. So until this is fixed, we're resorting to a very lo-fi way of checking for an update.
+
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+            string manifestFile = webClient.DownloadString(updateLocation);
+
+            //We have some garbage info from the file header, presumably because the file is a .application and not .xml
+            //Just start from the start of the first tag
+            int startOfXml = manifestFile.IndexOfAny(new[] { '<' });
+            manifestFile = manifestFile.Substring(startOfXml);
+
+            Version version;
+
+            XmlDocument doc = new XmlDocument();
+
+            //build the xml from the manifest
+            doc.LoadXml(manifestFile);
+
+            XmlNodeList nodesList = doc.GetElementsByTagName("assemblyIdentity");
+            if (nodesList == null || nodesList.Count <= 0)
+            {
+                throw new XmlException("Could not read the xml manifest file, which is required to check if an update is available.");
+            }
+
+            XmlNode theNode = nodesList[0];
+            version = new Version(theNode.Attributes["version"].Value);
+
+            Debug.WriteLine($"Versão atual: {ApplicationDeployment.CurrentDeployment.CurrentVersion}, versão do servidor: {version}");
+
+            if (version > ApplicationDeployment.CurrentDeployment.CurrentVersion)
+            {
+                // update application
+                return true;
+            }
+            return false;
+        }
 
         private void CheckAndInstallUpdates()
         {
@@ -521,48 +564,51 @@ namespace RepertorizacaoHome.src
 
                 if (ApplicationDeployment.IsNetworkDeployed)
                 {
-                    ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
+                    //ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
 
-                    try
-                    {
-                        // Setup the trust level
-                        var deployment = ApplicationDeployment.CurrentDeployment;
-                        var appId = new ApplicationIdentity(deployment.UpdatedApplicationFullName);
-                        var unrestrictedPerms = new PermissionSet(PermissionState.Unrestricted);
-                        var appTrust = new ApplicationTrust(appId)
-                        {
-                            DefaultGrantSet = new PolicyStatement(unrestrictedPerms),
-                            IsApplicationTrustedToRun = true,
-                            Persist = true
-                        };
-                        ApplicationSecurityManager.UserApplicationTrusts.Add(appTrust);
+                    //try
+                    //{
+                    //    // Setup the trust level
+                    //    var deployment = ApplicationDeployment.CurrentDeployment;
+                    //    var appId = new ApplicationIdentity(deployment.UpdatedApplicationFullName);
+                    //    var unrestrictedPerms = new PermissionSet(PermissionState.Unrestricted);
+                    //    var appTrust = new ApplicationTrust(appId)
+                    //    {
+                    //        DefaultGrantSet = new PolicyStatement(unrestrictedPerms),
+                    //        IsApplicationTrustedToRun = true,
+                    //        Persist = true
+                    //    };
+                    //    ApplicationSecurityManager.UserApplicationTrusts.Add(appTrust);
 
-                        info = ad.CheckForDetailedUpdate();
+                    //    info = ad.CheckForDetailedUpdate();
 
-                    }
-                    catch (DeploymentDownloadException dde)
-                    {
-                        Debug.WriteLine("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message);
-                        return;
-                    }
-                    catch (InvalidDeploymentException ide)
-                    {
-                        Debug.WriteLine("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message);
-                        return;
-                    }
-                    catch (InvalidOperationException ioe)
-                    {
-                        Debug.WriteLine("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message);
-                        return;
-                    }
-                    catch(Exception e)
-                    {
-                        Debug.WriteLine("General Error: " + e.Message);
-                        return;
-                    }
+                    //}
+                    //catch (DeploymentDownloadException dde)
+                    //{
+                    //    Debug.WriteLine("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message);
+                    //    return;
+                    //}
+                    //catch (InvalidDeploymentException ide)
+                    //{
+                    //    Debug.WriteLine("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message);
+                    //    return;
+                    //}
+                    //catch (InvalidOperationException ioe)
+                    //{
+                    //    Debug.WriteLine("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message);
+                    //    return;
+                    //}
+                    //catch(Exception e)
+                    //{
+                    //    Debug.WriteLine("General Error: " + e.Message);
+                    //    return;
+                    //}
 
-                    if (info.UpdateAvailable)
+                    if (CheckForUpdateAvailable())
                     {
+
+                        ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
+
                         Debug.WriteLine("Update encontrado");
 
                         if(await CustomMessageBox.Show("Existe uma atualização pendente para este software, deseja realizar a isntalação agora?") == CustomMessageBox.MessageRetuns.Yes)
